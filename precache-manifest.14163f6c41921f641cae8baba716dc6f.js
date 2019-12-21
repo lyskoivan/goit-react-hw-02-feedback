67164bf4e72da094c0fe032a017009003cb34d70
lyskoivan <lysko1999i@gmail.com>
2019-12-21 20:21:30 +0200

    Updates

---
+++ precache-manifest.14163f6c41921f641cae8baba716dc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986de517c658e8a5296e653cf61463b4",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "1a3bbe6801e32cfd7593",
+    "url": "/goit-react-hw-02-feedback/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1a3bbe6801e32cfd7593",
+    "url": "/goit-react-hw-02-feedback/static/js/2.baf48665.chunk.js"
+  },
+  {
+    "revision": "497d7e07705261348096eb742962e0f0",
+    "url": "/goit-react-hw-02-feedback/static/js/2.baf48665.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de6f757ed1ae8b0324fe",
+    "url": "/goit-react-hw-02-feedback/static/js/main.3797b6fb.chunk.js"
+  },
+  {
+    "revision": "6d4883b48959809198b2",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.f22f9b33.js"
+  }
+]);
